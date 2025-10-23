cba28b70791820e1cf27babb8d1f67206f14d926
Sahid Rojas Chacon <sahid.rojas64@gmail.com>
2025-10-22 20:30:22 -0600

    Se creo el componente Tarjeta

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Tarjeta } from './tarjeta/tarjeta';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Tarjeta],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/tarjeta/tarjeta.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarjeta',
+  imports: [],
+  templateUrl: './tarjeta.html',
+  styleUrl: './tarjeta.css',
+})
+export class Tarjeta {
+
+}
